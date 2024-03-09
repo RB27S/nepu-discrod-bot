9b35ec79c9ead18ea3ee07b028a72bee91c4b50f
Rafa <rbutiragoserrano@gmail.com>
2024-03-09 23:30:47 +0100

    remove comments

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,5 @@
 const {Events, ActivityType} = require('discord.js')
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
