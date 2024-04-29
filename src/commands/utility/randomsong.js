7037b1bb5a254b1e9670946b479e26b3f5a4ac0c
Rafa <rbutiragoserrano@gmail.com>
2024-04-29 14:00:42 +0200

    fix randomsong command description

--- src/commands/utility/randomsong.js
+++ src/commands/utility/randomsong.js
@@ -4,7 +4,7 @@ const {getRandomSong} = require('../../services/spotify/getRandomSong')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('randomsong')
-		.setDescription('sugests a random song to listen to from my personal playlist'),
+		.setDescription('Recommends a random song to listen to from my personal playlist'),
 	async execute(interaction) {
 		const randomSong = await getRandomSong()
 
