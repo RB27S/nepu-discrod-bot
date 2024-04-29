69a7d268f97e74c35009cd49e688e7934a74fa98
Rafa <rbutiragoserrano@gmail.com>
2024-04-29 12:47:06 +0200

    Revert "for-loops refactor"
    This reverts commit 013bf59c44c38c89cbce12c79a11bf5ebfbdc0ea.

--- src/index.js
+++ src/index.js
@@ -15,11 +15,10 @@ client.cooldowns = new Collection()
 const foldersPath = path.join(__dirname, 'commands')
 const commandFolders = fs.readdirSync(foldersPath)
 
-commandFolders.forEach(folder => {
+for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder)
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
-
-	commandFiles.forEach(file => {
+	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file)
 		const command = require(filePath)
 		// Set a new item in the Collection with the key as the command name and the value as the exported module
@@ -28,22 +27,21 @@ commandFolders.forEach(folder => {
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
 		}
-	})
-})
+	}
+}
 
 const eventsPath = path.join(__dirname, 'events')
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))
 
-eventFiles.forEach(file => {
+for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file)
 	const event = require(filePath)
-
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args))
 	} else {
 		client.on(event.name, (...args) => event.execute(...args))
 	}
-})
+}
 
 // Log in to Discord with your client's token
 client.login(token)
